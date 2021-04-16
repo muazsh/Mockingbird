8400f2c59d31ea39d5139ad0d5f85c921ceefacb
MOUAZ\Mouaz <moaazshamia@gmail.com>
2021-04-16 23:23:56 +0200

    overloading

--- Mockingbird/Mockingbird.hpp
+++ Mockingbird/Mockingbird.hpp
@@ -32,6 +32,27 @@ ReturnType FuncName##Signature const override\
 { m_##FuncName##CallCounter++;\
 return m_##FuncName##Class.m_func(__VA_ARGS__);}
 
+#define OVERLOAD_INJECTION_SET(FuncName, Substitute, overloadedMethodNumber)\
+private:\
+FuncName##Class<decltype(Substitute)> m_##FuncName##Class##overloadedMethodNumber;\
+mutable int m_##FuncName##overloadedMethodNumber##CallCounter = 0;\
+public:\
+void Inject##FuncName(decltype(Substitute)){\
+m_##FuncName##Class##overloadedMethodNumber.m_func = Substitute;}\
+int Get##FuncName##overloadedMethodNumber##CallCounter(){return m_##FuncName##CallCounter;}
+
+#define METHOD_OVERLOAD_INJECTION_SET(FuncName,ReturnType, Signature, Substitute, overloadedMethodNumber, .../*signature variables*/)\
+OVERLOAD_INJECTION_SET(FuncName, Substitute, overloadedMethodNumber)\
+ReturnType FuncName##Signature override\
+{ m_##FuncName##overloadedMethodNumber##CallCounter++;\
+return m_##FuncName##Class##overloadedMethodNumber.m_func(__VA_ARGS__);}
+
+#define CONST_METHOD_OVERLOAD_INJECTION_SET(FuncName,ReturnType, Signature, Substitute, overloadedMethodNumber, .../*signature variables*/)\
+OVERLOAD_INJECTION_SET(FuncName, Substitute, overloadedMethodNumber)\
+ReturnType FuncName##Signature const override\
+{ m_##FuncName##overloadedMethodNumber##CallCounter++;\
+return m_##FuncName##Class##overloadedMethodNumber.m_func(__VA_ARGS__);}
+
 #define END_MOCK(MockingClass)\
 };
 
--- MockingbirdTest/test.cpp
+++ MockingbirdTest/test.cpp
@@ -9,6 +9,7 @@ class Foo{
 public:
 	virtual void SetMyStruct(MyStruct& myStruct) = 0;
 	virtual const MyStruct CreateMyStruct(int x, int y) { return MyStruct{ x,y }; };
+	virtual const MyStruct CreateMyStruct(int x) { return MyStruct{ x,x }; };
 	virtual MyStruct MakeSpecialCopyMyStruct(const std::shared_ptr<MyStruct>& myStruct) const { return MyStruct{ myStruct->x, myStruct->y }; }
 	virtual int GetTen() { return 10; }
 };
@@ -24,11 +25,13 @@ MyStruct GetMyStructFoo(const std::unique_ptr<Foo>& foo, int x, int y){
 #pragma region Mocking Fixture
 void SetMyStructSubstitute(MyStruct& myStruct) { myStruct.x = 10; myStruct.y = 10; }
 const MyStruct CreateMyStructSubstitute(int x, int y) { return MyStruct{ x + 10, y + 10 }; }
+const MyStruct CreateMyStructSubstitute2(int x) { return MyStruct{ x + 5, x + 5 }; }
 MyStruct MakeSpecialCopyMyStructSubstitute(const std::shared_ptr<MyStruct>& myStruct) { return MyStruct{ myStruct->x + 10, myStruct->y + 10 }; } // This is a static method wihch cannot be const
 
 START_MOCK(FooMock, Foo)
 METHOD_INJECTION_SET(SetMyStruct, void, (MyStruct& myStruct), &SetMyStructSubstitute, myStruct)
 METHOD_INJECTION_SET(CreateMyStruct, const MyStruct, (int x, int y), &CreateMyStructSubstitute, x, y)
+METHOD_OVERLOAD_INJECTION_SET(CreateMyStruct, const MyStruct, (int x), &CreateMyStructSubstitute2, 1, x)
 CONST_METHOD_INJECTION_SET(MakeSpecialCopyMyStruct, MyStruct, (const std::shared_ptr<MyStruct>& myStruct), &MakeSpecialCopyMyStructSubstitute, myStruct)
 END_MOCK(FooMock)
 #pragma endregion
@@ -49,6 +52,19 @@ TEST(Mockingbird, ConstReturnPrimitivesSignature){
 	auto createdMyStruct = fooMock.CreateMyStruct(10, 10);
 	EXPECT_EQ(createdMyStruct.x, 20);
 	EXPECT_EQ(createdMyStruct.y, 20);
+}
+
+TEST(Mockingbird, Overloading) {
+	MyStruct myStruct{ 1, 1 };
+	FooMock fooMock;
+	fooMock.InjectCreateMyStruct(&CreateMyStructSubstitute); // Mocking methods injection.
+	fooMock.InjectCreateMyStruct(&CreateMyStructSubstitute2); // Mocking methods injection.
+	auto createdMyStruct1 = fooMock.CreateMyStruct(10, 10);
+	EXPECT_EQ(createdMyStruct1.x, 20);
+	EXPECT_EQ(createdMyStruct1.y, 20);
+	auto createdMyStruct2 = fooMock.CreateMyStruct(0);
+	EXPECT_EQ(createdMyStruct2.x, 5);
+	EXPECT_EQ(createdMyStruct2.y, 5);
 }
 
 TEST(Mockingbird, ConstMethodConstRefPointerSignature){
